e3be23f474781a87de21db48c607417000d73517
mars1385 <faceless1385@gmail.com>
2019-08-18 11:33:56 +0430

    done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//
+//google map
 function initMap() {
 	const loc = { lat: -34.397, lng: 150.644 };
 	const map = new google.maps.Map(document.querySelector('.map'), {
@@ -8,3 +8,26 @@ function initMap() {
 
 	const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+//scroll opacity
+window.addEventListener('scroll', function() {
+	if (window.scrollY > 150) {
+		this.document.querySelector('#navbar').style.opacity = 0.8;
+	} else {
+		this.document.querySelector('#navbar').style.opacity = 1;
+	}
+});
+
+//scrolling
+$('#navbar a, .btn').on('click', function(e) {
+	if (this.hash !== '') {
+		e.preventDefault();
+		const hash = this.hash;
+		$('html , body').animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+	}
+}); //end
